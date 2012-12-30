7fed82c39dca3422bf4bb1aab37dfe6ebf89214b
Mark Villacampa <markvjal@gmail.com>
2012-12-30 18:29:07 +0100

    Initial Commit

---
+++ MotionCpp/MotionCpp.h
@@ -0,0 +1,13 @@
+//
+//  MotionCpp.h
+//  MotionCpp
+//
+//  Created by Marcos Villacampa on 12/30/12.
+//  Copyright (c) 2012 Mark Villacampa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MotionCpp : NSObject
+
+@end
---
+++ MotionCpp/MotionCpp.m
@@ -0,0 +1,13 @@
+//
+//  MotionCpp.m
+//  MotionCpp
+//
+//  Created by Marcos Villacampa on 12/30/12.
+//  Copyright (c) 2012 Mark Villacampa. All rights reserved.
+//
+
+#import "MotionCpp.h"
+
+@implementation MotionCpp
+
+@end
