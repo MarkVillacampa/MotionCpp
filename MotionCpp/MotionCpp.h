4db6f5d13974db851a4c7bfc13e8f84d9d2e9790
Mark Villacampa <markvjal@gmail.com>
2012-12-30 18:58:18 +0100

    intitial commmit

--- MotionCpp/MotionCpp.h
+++ MotionCpp/MotionCpp.h
@@ -2,12 +2,27 @@
 //  MotionCpp.h
 //  MotionCpp
 //
-//  Created by Marcos Villacampa on 12/30/12.
+//  Created by Mark Villacampa on 12/30/12.
 //  Copyright (c) 2012 Mark Villacampa. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
+// We define our cpp class as a struct so that Xcode won't complaint about missing definitions if we dont include the .h file here
+struct myCppClass;
+
 @interface MotionCpp : NSObject
+{
+  // myApp is the instance variable that will hold our Cpp object
+  // never EVER try to access this variable directly from RubyMotion, or it will crash
+  myCppClass* myApp;
+}
+
+
+// We will define Objc methods that will call to the corresponding cpp methods. These can return primitive C or Objc types, but never Cpp class objects.
+// For some reason, just having a reference in your ruby code to an objc method that would return a Cpp object will make the runtime go crazy and spit all kind of "missing stub" errors.
+- (void)myObjcMethod;
+
+@property struct myCppClass* myApp;
 
 @end
---
+++ MotionCpp/MotionCpp.mm
@@ -0,0 +1,38 @@
+//
+//  MotionCpp.m
+//  MotionCpp
+//
+//  Created by Mark Villacampa on 12/30/12.
+//  Copyright (c) 2012 Mark Villacampa. All rights reserved.
+//
+
+#import "MotionCpp.h"
+
+// Here we will include the Cpp header files where our classes are defined.
+// Make sure you have currectly setup "Search Paths > Header Search Paths" under Build Settings
+#include "myCppHeader.h"
+
+
+@implementation MotionCpp
+
+@synthesize app;
+
+- (id)init
+{
+  self = [super init];
+  if (self)
+  {
+    // We initialize our Cpp class and assign it to our instace variable
+    self.myApp = new vesKiwiViewerApp;
+  }
+  
+  return self;
+}
+
+// We create objc methods that map to the different Cpp methods
+- (void) myObjcMethod
+{
+  self.app->MyCppMethod();
+}
+
+@end
